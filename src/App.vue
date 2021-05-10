48b884372c3d4930c13d0001e1b7d62ef9362e32
Swamy-ai <72182728+Swamy-ai@users.noreply.github.com>
2021-05-10 18:01:04 +0530

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-app>
+  <v-card
+    color="grey lighten-4"
+    flat
+    height="60px"
+    tile
+    app
+  >
+    <v-toolbar
+    >
+      <div class="pl-5 d-flex align-center">
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="120"
+          src="./components/Picture1.png"
+          width="150"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+     <div class="pl-5 d-flex align-center">
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="120"
+          src="./components/Picture2.png"
+          width="150"
+        />
+      </div>
+    </v-toolbar>
+  </v-card>
+
+    <v-main class="" height="100vh">
+      <login />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import login from './components/login/login.vue'
+
+export default {
+  name: 'App',
+  components:{
+    login,
+  }
+};
+</script>
